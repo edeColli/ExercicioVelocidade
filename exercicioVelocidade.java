99dc7c6d6431b97e90118323a8d3f6ac309a8594
Ednesio Colli <ednesio.colli@gmail.com>
2023-01-21 07:39:56 -0300

    Ajustes na validação do radar

--- exercicioVelocidade.java
+++ exercicioVelocidade.java
@@ -1,21 +1,44 @@
 import java.util.Scanner;
 
+import javax.print.event.PrintJobListener;
+
+import java.util.*;
+
 public class exercicioVelocidade {
+
   public static void main(String args[]) {
-    Scanner entrada = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
+    int velocidadeVeiculo;
+    int velocidadeMaxima;
 
     System.out.print("Informe a velocidade do veículo: ");
-    int velocidadeVeiculo = entrada.nextInt();
+    velocidadeVeiculo = validarValorInteiro(sc);
+    System.out.print("Informe a velocidade máxima: ");
+    velocidadeMaxima = validarValorInteiro(sc);
 
-    System.out.print("Informe a velocidade máxima permitida: ");
-    int velocidadeMaxima = entrada.nextInt();
-
-    velocidadeMaxima = velocidadeMaxima + (velocidadeMaxima / 10);
-    if (velocidadeVeiculo > velocidadeMaxima) {
-      System.out.println("Multado!");
+    if (velocidadeVeiculo != 0 & velocidadeMaxima != 0) {
+      velocidadeMaxima = velocidadeMaxima + (velocidadeMaxima / 10);
+      if (velocidadeVeiculo > velocidadeMaxima) {
+        System.out.println("Multado!");
+      } else {
+        System.out.println("OK!");
+      }
     } else {
-      System.out.println("OK!");
+      System.out.println("Não foi possível aferir a velocidade");
     }
-    entrada.close();
+    sc.close();
+  }
+
+  private static int validarValorInteiro(Scanner sc) {
+    int valorInteiro = 0;
+
+    if (sc.hasNextInt()) {
+      valorInteiro = sc.nextInt();
+    }
+
+    if (valorInteiro == 0) {
+      System.out.println("Velocidade informada é inválida!");
+    }
+    return valorInteiro;
   }
 }
